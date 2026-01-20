e6a93462643015f0031e6a04f2781b13d5fdf007
Gustavo Leal <fortreu@outlook.com>
2026-01-20 14:14:04 -0300

    Adição dos pacotes Controller,Model e service. E criação das classes Cliente e Endereço com seus respectivos repositories.

---
+++ src/main/java/dio_project/padroes/controller/Cliente.java
@@ -0,0 +1,37 @@
+package dio_project.padroes.controller;
+
+import jakarta.persistence.*;
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    private Endereco endereco;
+}
---
+++ src/test/java/dio_project/padroes/PadroesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dio_project.padroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
